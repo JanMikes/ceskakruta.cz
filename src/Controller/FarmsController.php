2cd0da55b5439be62f0f3b5f51caa660e6bb50f6
Jan Mikeš <j.mikes@me.com>
2024-04-21 16:04:37 +0200

    Draft: Sales places + farms pages

---
+++ src/Controller/FarmsController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CeskaKruta\Web\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class FarmsController extends AbstractController
+{
+    #[Route(path: '/farmy-ceske-kruty', name: 'farms', methods: ['GET'])]
+    public function __invoke(): Response
+    {
+        return $this->render('farms.html.twig');
+    }
+}
