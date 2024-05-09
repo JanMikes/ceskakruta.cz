81952d7a574df0e694831781b68d917a190dc2ad
Jan Mikeš <j.mikes@me.com>
2024-05-09 23:23:52 +0200

    Choose pickup place

--- config/packages/framework.php
+++ config/packages/framework.php
@@ -20,5 +20,8 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ],
         'trusted_headers' => ['x-forwarded-for', 'x-forwarded-host', 'x-forwarded-proto', 'x-forwarded-port', 'x-forwarded-prefix'],
         'trusted_proxies' => '127.0.0.1,REMOTE_ADDR',
+        'fragments' => [
+            'path' => '_fragments'
+        ],
     ]);
 };
