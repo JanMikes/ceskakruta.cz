b1566f96b77c085d847c56732c887b72a8eb42d3
Jan Mikeš <j.mikes@me.com>
2024-05-13 21:06:02 +0200

    PHPStan fix

--- src/MessageHandler/ChooseDeliveryHandler.php
+++ src/MessageHandler/ChooseDeliveryHandler.php
@@ -6,17 +6,11 @@ namespace CeskaKruta\Web\MessageHandler;
 
 use CeskaKruta\Web\Exceptions\UnsupportedDeliveryToPostalCode;
 use CeskaKruta\Web\Message\ChooseDelivery;
-use CeskaKruta\Web\Services\Cart\CartStorage;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
 final class ChooseDeliveryHandler
 {
-    public function __construct(
-        private CartStorage $cartStorage,
-    ) {
-    }
-
     /**
      * @throws UnsupportedDeliveryToPostalCode
      */
