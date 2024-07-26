759440b975b48485de1f95f5c0ee01053cf13414
Jan Mikeš <j.mikes@me.com>
2024-07-26 16:31:41 +0200

    Fix mailers

--- config/packages/mailer.php
+++ config/packages/mailer.php
@@ -6,14 +6,9 @@ use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
 use Symfony\Config\FrameworkConfig;
 
 return static function (FrameworkConfig $framework): void {
-    $mailerConfig = $framework->mailer();
-
-    $mailerConfig->transport('main', env('MAILER_DSN'))
+    $framework->mailer()
+        ->transport('main', env('MAILER_DSN'))
+        ->transport('orders', env('MAILER_ORDERS_DSN'))
         ->envelope()
-        ->sender('info@ceskakruta.cz');
-
-    $mailerConfig->transport('orders', env('MAILER_ORDERS_DSN'))
-        ->envelope()
-        ->sender('objednavky@ceskakruta.cz');
-
+            ->sender('info@ceskakruta.cz');
 };
