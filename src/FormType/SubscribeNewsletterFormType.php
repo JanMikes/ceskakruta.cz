df4a6a019c639a9ff7836d7ffcffb9a5a2dd2137
Jan Mikeš <j.mikes@me.com>
2024-09-12 22:24:42 +0200

    Newsletter Agreement added

--- src/FormType/SubscribeNewsletterFormType.php
+++ src/FormType/SubscribeNewsletterFormType.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 namespace CeskaKruta\Web\FormType;
 
 use CeskaKruta\Web\FormData\SubscribeNewsletterFormData;
+use Symfony\Component\DependencyInjection\Compiler\CheckAliasValidityPass;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,6 +19,12 @@ final class SubscribeNewsletterFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->add('email', EmailType::class);
+
+        $builder->add('agreement', CheckboxType::class, [
+            'required' => true,
+            'label' => 'Souhlasím se zasíláním novinek',
+            'mapped' => false,
+        ]);
     }
 
 
